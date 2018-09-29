885de88006bb88e5bf23993511d27cfc8cee75f1
Tom <thomas.bouttaz@gmail.com>
2018-09-30 01:18:51 +0200

    Adding static images in SurveyAnswer component

--- src/main/webapp/app/entities/survey/survey-answer.tsx
+++ src/main/webapp/app/entities/survey/survey-answer.tsx
@@ -5,6 +5,7 @@ import { Button, Row, Col } from 'reactstrap';
 // tslint:disable-next-line:no-unused-variable
 import { Translate, ICrudGetAction } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {RadioGroup, Radio} from 'react-radio-group'
 
 import { IRootState } from 'app/shared/reducers';
 import { getEntity } from './survey.reducer';
@@ -39,6 +40,19 @@ export class SurveyAnswer extends React.Component<ISurveyAnswerProps> {
                 Which picture do you prefer?
               </span>
             </dt>
+            <dd>
+              {/* TODO - use Flikr API?
+              http://cassandra.gelvins.com/tools/randm-flickr-pickr/
+              https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d61091b4772d0e40c3743b6a5fc54084&license=&min_upload_date=1536113113&max_upload_date=1536120313&orientation=landscape,portrait,square,panorama&safe_search=1&url_o&per_page=100&format=json&nojsoncallback=1
+              https://www.flickr.com/services/api/flickr.collections.getTree.html
+              https://www.flickr.com/photos/{user-id}/{photo-id} - individual photo
+              https://www.flickr.com/services/api/misc.urls.html */}
+
+            <RadioGroup name="match1">
+              <Radio value="photo1" />GOPR0111_0853 <img src="content/images/survey/GOPR0111_0853.png"></img>
+              <Radio value="photo2" />GOPR0111_2136 <img src="content/images/survey/GOPR0111_2136.png"></img>
+            </RadioGroup>
+            </dd>
             <dt>
               <span id="embededForm">
                 Please answer the following questions:
