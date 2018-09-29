0337d65ee8288866dcb2954c187eeda3ce9d929f
Tom <thomas.bouttaz@gmail.com>
2018-09-30 00:29:39 +0200

    Creating component to answer a survey

--- src/main/webapp/app/entities/survey/index.tsx
+++ src/main/webapp/app/entities/survey/index.tsx
@@ -7,6 +7,7 @@ import Survey from './survey';
 import SurveyDetail from './survey-detail';
 import SurveyUpdate from './survey-update';
 import SurveyDeleteDialog from './survey-delete-dialog';
+import SurveyAnswer from './survey-answer';
 
 const Routes = ({ match }) => (
   <>
@@ -14,6 +15,7 @@ const Routes = ({ match }) => (
       <ErrorBoundaryRoute exact path={`${match.url}/new`} component={SurveyUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={SurveyUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={SurveyDetail} />
+      <ErrorBoundaryRoute exact path={`${match.url}/:id/answer`} component={SurveyAnswer} />
       <ErrorBoundaryRoute path={match.url} component={Survey} />
     </Switch>
     <ErrorBoundaryRoute path={`${match.url}/:id/delete`} component={SurveyDeleteDialog} />
